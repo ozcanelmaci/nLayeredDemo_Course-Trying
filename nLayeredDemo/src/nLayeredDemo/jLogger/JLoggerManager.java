a244e6c086f09b174c6a0ce49c00821829bd52df
Özcan Elmacı <ozcan.elmaci@ogr.deu.edu.tr>
2022-03-16 20:12:46 +0300

    Java Kurumsal Mimarilere Giriş

---
+++ nLayeredDemo/src/nLayeredDemo/jLogger/JLoggerManager.java
@@ -0,0 +1,7 @@
+package nLayeredDemo.jLogger; //bu paketi bizim bilmediðimiz birinin yazdýðýný düþünüyoruz ve onu adapte etmeye çalýþýyoruz.
+
+public class JLoggerManager {
+	public void log(String message) {
+		System.out.println("J logger ile loglandý " + message);
+	}
+}
